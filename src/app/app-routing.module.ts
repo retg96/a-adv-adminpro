75157f0dd9f8845d834270ba5fb840dbc390dce7
Ruben Glz <retg1996@gmail.com>
2021-08-09 20:30:18 -0500

    fin seccion 4- router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { AuthRoutingModule } from './auth/auth.routing';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+
 
 //Configurar las rutas de mi app
 const routes: Routes =[
-  //rutas protegidas
-  {
-    path:'', 
-    component: PagesComponent,
-
-    //rutas hijas
-    children:[
-      {path: 'dashboard', component: DashboardComponent},
-      {path: 'progress', component: ProgressComponent},
-      {path: 'grafica1', component: Grafica1Component},
-      {path: '', redirectTo:'/dashboard', pathMatch:'full'},
-    ]
-  },
+  
 
   //rutas publicas
-  {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent},
 
+  {path: '', redirectTo:'/dashboard', pathMatch:'full'},
   {path: '**', component: NopagefoundComponent},
 
 ];
@@ -35,7 +19,9 @@ const routes: Routes =[
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [RouterModule]
 })
